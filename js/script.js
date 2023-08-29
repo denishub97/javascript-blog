05699c2efbbe3f63da4cfe2e50b848b8082815ad
Denis <denis97@wp.pl>
2023-08-29 18:20:06 +0200

    eslint

--- js/script.js
+++ js/script.js
@@ -9,58 +9,50 @@ function titleClickHandler(event){
   console.log(event);
 
   /* [DONE]remove class 'active' from all article links  */
-const activeLinks = document.querySelectorAll('.titles a.active');
+  
 
-for(let activeLink of activeLinks){
-  activeLink.classList.remove('active');
-}
+  const activeLinks = document.querySelectorAll('.titles a.active');
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }  
   /* [DONE]add class 'active' to the clicked link */
-console.log('clickedElement:', clickedElement);
-clickedElement.classList.add('active');
-
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
   /* [DONE]remove class 'active' from all articles */
-const activeArticles = document.querySelectorAll('.post.active');
-
-for(let activeArticle of activeArticles){
-  activeArticle.classList.remove('active');
-}
+  const activeArticles = document.querySelectorAll('.post.active');
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
   /* [DONE]get 'href' attribute from the clicked link */
-const articleSelector = clickedElement.getAttribute('href');
-console.log(articleSelector);
-
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
   /* [DONE]find the correct article using the selector (value of 'href' attribute) */
-const href = clickedElement.getAttribute('href')
-const targetArticle = document.querySelector(href);
-
+  const href = clickedElement.getAttribute('href');
+  const targetArticle = document.querySelector(href);
   console.log(targetArticle);
   /* [DONE]add class 'active' to the correct article */
   targetArticle.classList.add('active');
 }
 
-  const links = document.querySelectorAll('.titles a');
-
-  for(let link of links){
+const links = document.querySelectorAll('.titles a');
+for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
   
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
-  optArticleAuthorSelector ='.post-author',
-  optTagsListSelector ='.tags .list',
   optTitleListSelector = '.titles'; 
 
 
 function generateTitleLinks(customSelector = ''){
-
-  /* [DONE]remove contents of titleList */
+/* [DONE]remove contents of titleList */
   const titleList = document.querySelector(optTitleListSelector);
-  console.log("title list " + titleList);
+  console.log('title list' + titleList);
 
   titleList.innerHTML = '';
   
   /* [DONE]find all the articles and save them to variable: articles */
   /* ... */
-  
   const articles = document.querySelectorAll(optArticleSelector + customSelector);
 
   let html ='';
